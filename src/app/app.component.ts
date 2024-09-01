223b71f17867efe6deedc7159c21ea5a52f2a3bc
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-01 11:45:59 +0530

    fix: reusable components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { SupportTicketsComponent } from "./dashboard/support-tickets/support-tickets.component";
+import { DashboardItemComponent } from "./dashboard/shared/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, SupportTicketsComponent, DashboardItemComponent],
 })
 export class AppComponent {
 
