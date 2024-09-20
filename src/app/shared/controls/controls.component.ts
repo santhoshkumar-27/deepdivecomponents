2854b322eb1fa5927ae99858d7b74fac5f4f6827
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-20 16:22:42 +0530

    fix: added host element class like property from the component its self

--- src/app/shared/controls/controls.component.ts
+++ src/app/shared/controls/controls.component.ts
@@ -6,7 +6,11 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './controls.component.html',
   styleUrl: './controls.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  // we can add host element class from here without going to put in each of these selection where we used
+  host: {
+    class: 'control'
+  }
 })
 export class ControlsComponent {
 
