da62c37bfd31653b759243cc30ea5b710ed4a8f7
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-20 18:01:48 +0530

    fix: comments added and disabled

--- src/app/shared/controls/controls.component.ts
+++ src/app/shared/controls/controls.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-controls',
@@ -8,14 +8,20 @@ import { Component, HostBinding, input, ViewEncapsulation } from '@angular/core'
   styleUrl: './controls.component.css',
   encapsulation: ViewEncapsulation.None,
   // we can add host element class from here without going to put in each of these selection where we used
-  host: {
-    class: 'control'
+  host: { // but the property values are static if you want to change dynamically we use hostbinding
+    class: 'control', // replacement for host binding
+    // '(click)': 'onClick()' // replacement for host listener
   }
 })
 export class ControlsComponent {
-
   // @HostBinding('class') className = 'control'; // it will add these property bind with the host element
 
   label = input.required<string>()
+
+  // it will add the listner to the host element using the host listener
+  // @HostListener('click')
+  // onClick() {
+  //   console.log('clicked')
+  // }
 
 }
