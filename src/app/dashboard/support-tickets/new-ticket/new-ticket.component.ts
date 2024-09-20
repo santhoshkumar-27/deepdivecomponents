544967b3baaa8f20c87e75b10e5450b099bdf4df
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-20 14:59:10 +0530

    fix: complex template projection

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ControlsComponent } from "../../../shared/controls/controls.component";
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlsComponent],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
---
+++ src/app/shared/controls/controls.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-controls',
+  standalone: true,
+  imports: [],
+  templateUrl: './controls.component.html',
+  styleUrl: './controls.component.css'
+})
+export class ControlsComponent {
+
+  label = input.required<string>()
+
+}
