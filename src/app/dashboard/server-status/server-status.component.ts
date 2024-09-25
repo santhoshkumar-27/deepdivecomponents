fe50835ebfb381a128e2964dab0d50274f3026ed
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-25 11:04:18 +0530

    fix: interface added

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -8,6 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: SERVERSTATUS = SERVERSTATUS.ONLINE;
 
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = SERVERSTATUS.ONLINE;
+      } else if (rnd < 0.9) {
+        this.currentStatus = SERVERSTATUS.OFFLINE;
+      } else {
+        this.currentStatus = SERVERSTATUS.UNKNOWN;
+      }
+
+    }, 5000)
+  }
+}
+
+
+
+enum SERVERSTATUS {
+  ONLINE = 'online',
+  OFFLINE = 'offline',
+  UNKNOWN = 'unknown'
 }
