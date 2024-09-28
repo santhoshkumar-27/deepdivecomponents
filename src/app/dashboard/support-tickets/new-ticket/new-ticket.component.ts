f392eda64a850b58be1c1165cea448f876af0e89
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-28 20:25:57 +0530

    fix: form submitted

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlsComponent } from "../../../shared/controls/controls.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlsComponent],
+  imports: [ButtonComponent, ControlsComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
 
+
+  onFormSubmit() {
+    console.log('form is submitted')
+  }
 }
