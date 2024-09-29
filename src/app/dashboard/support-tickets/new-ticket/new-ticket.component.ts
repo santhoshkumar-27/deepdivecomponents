370d42cc38320e81ba0cd980daf38606658be077
santhoshkumar <its.santhoshkumar@outlook.com>
2024-09-29 14:50:15 +0530

    feat: implementing lifecycles

--- src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild, viewChildren, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild, viewChildren, ViewChildren } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlsComponent } from "../../../shared/controls/controls.component";
 import { FormsModule } from '@angular/forms';
@@ -10,12 +10,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit {
 
   // @ViewChild('form') form!:  ElementRef<HTMLFormElement> ;
   // private form = viewChild<ElementRef<HTMLFormElement>>('form');
   // template value will definitely found
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit(): void {
+    // console.log(this.form())
+  }
+
+  ngAfterViewInit(): void {
+    // console.log(this.form())
+  }
 
   onFormSubmit(title: string, request: string) {
     console.log('form is submitted');
